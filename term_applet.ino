288edd2cfa92a6d622bab72bff20192f07871779
Ricardo Cabrita <ricardocabrita@MacBook-Pro-de-Ricardo.local>
2020-07-09 15:43:17 +0100

    Quick sketch to test the SPI pins

--- term_applet.ino
+++ term_applet.ino
@@ -17,6 +17,9 @@ size_t nr_bytes;
 //pin out definitions
 const int ss_pin = 10;
 const int ad5293rdy = 8;
+int kk = 0;
+
+int pins [5] = {8,12,10,13,11};
 
 void read_byte(char c) {
   if (c == '\n') {
@@ -44,35 +47,13 @@ void try_execute() {
     //Serial.println(cmd[i]);
   }
   cmd[len] = '\0'; //strings end with \0
-  String scmd = cmd;
-  String subzero = scmd.substring(0,4);
 
   //string cmp with commands
 
-  if(len < 4) {
-    int speed = atoi(cmd);
-    if (speed > -1 && speed < 256){
-        Serial.print("Setting motor speed to: ");
-        Serial.println(speed);
-        pump.setSpeed(speed);
-    }
-  }
-  else if (subzero == "pot ") {
-    Serial.println("Got a pot command");
-    int val = scmd.substring(4).toInt();
-    Serial.print("- setting wiper to: ");
-    Serial.println(val);
-    //ad5293potWrite(val);  
-  }
-  else if (strcmp(cmd, "fill") == 0) {
-    pump.run(FORWARD);
-    Serial.println("Look at me I'm filling the sphere!");
-  } else if (strcmp(cmd, "empty") == 0) {
-    pump.run(BACKWARD);
-    Serial.println("Look at me I'm emptying the sphere!");
-  } else if (strcmp(cmd, "stop") == 0) {
-    pump.run(RELEASE);
-    Serial.println("Look at me I'm stopping the pump");
+  if (strcmp(cmd, "check") == 0) {
+    Serial.println("Yes, the serial cmd line is working");
+  } else {
+    Serial.pintln("Testing pin..");
   }
   go--;
 
@@ -85,8 +66,11 @@ void setup() {
   error_flag = false;
 
   //SPI setup for AD5293
-  pinMode(ss_pin, OUTPUT);
-  pinMode(ad5293rdy, INPUT);
+  //pinMode(ss_pin, OUTPUT);
+  //pinMode(ad5293rdy, INPUT);
+  for (int i = 0; i < 5; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
 
   Serial.begin(115200);
 
@@ -100,47 +84,12 @@ void loop() {
 
   if (go > 0)
     try_execute();
+    digitalWrite(pins[kk], HIGH);
+    Serial.print("Turning pin ");
+    Serial.print(kk);
+    Serial.println(" HIGH");
+    delay(5000); //wait 5 seconds
+    digitalWrite(pins[kk], LOW);
+    kk++;
 
-}
-
-
-//AD5293 rheostat programming functions
-void ad5293EnableWrite() {
-  SPI.beginTransaction(SPISettings(SPI_CLOCK_DIV4, MSBFIRST, SPI_MODE1));
-
-  digitalWrite(ss_pin, LOW);
-  //is a delay necessary here ? probably..
-  delay(100); //100ms delay.. could also monitor the RDY pin ?
-  SPI.transfer(0x18); //Command 4: 0001 10xx
-  SPI.transfer(0x02); //XXXX X01X
-  digitalWrite(ss_pin, HIGH);
-
-  SPI.endTransaction();
-}
-
-void ad5293potWrite(unsigned int val) {
-  SPI.beginTransaction(SPISettings(SPI_CLOCK_DIV4, MSBFIRST, SPI_MODE1));
-
-  digitalWrite(ss_pin, LOW);
-
-  //val >> 8 2 higher bits of data + command
-  byte hibyte = (val >> 8) + 0x18; //example had + 0x04, but that can be right, right ?
-  byte lobyte = val & 0xFF;
-  SPI.transfer(hibyte);
-  SPI.transfer(lobyte);
-  digitalWrite(ss_pin, HIGH);
-  SPI.endTransaction();
-}
-
-int ad5293readWiper() {
-  //probably not gonna work...
-  SPI.beginTransaction(SPISettings(SPI_CLOCK_DIV4, MSBFIRST, SPI_MODE1));
-
-  digitalWrite(ss_pin, LOW);
-  word cmd2 = word(0x08, 0x00);
-  int res = SPI.transfer16(cmd2);
-  int wiper = res & 0x3FF;
-  digitalWrite(ss_pin, HIGH);
-  SPI.endTransaction();
-  return wiper;
 }
